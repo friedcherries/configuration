9637e9b15288f4488d651e04fe519578588c20fa
Michael D. Culey <michael@culey.dev>
2023-11-03 00:28:03 -0500

    Updated bashrc.d script install

--- app-installs/rc-scripts.sh
+++ app-installs/rc-scripts.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-echo Checking tailscale status
-tailscale status > /dev/null
-if [ $? -ne 0 ]; then
-    echo Tailscale is not up and running. You need to setup tailscale and auth before continuing.
-    exit 66
-fi
-
-echo Install bashrc.d scripts
-mkdir -p ${HOME}/.bashrc.d
-scp hoth:deploy/homedir/.bashrc.d/* ${HOME}/.bashrc.d
-cat >> ${HOME}/.bashrc <<- EOM
+if [ ! -d ${HOME}/.bashrc.d ]; then
+    echo Install bashrc.d scripts
+    mkdir -p ${HOME}/.bashrc.d
+    cp bashrc.d/*.bashrc ${HOME}/.bashrc.d
+    cat >> ${HOME}/.bashrc <<- EOM
 for file in \$(ls \${HOME}/.bashrc.d/\*.bashrc); do
     source \${file};
 done
 EOM
+else
+    echo bashrc.d scripts already installed
+fi
